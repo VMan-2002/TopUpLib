f2c64656610d89918f44332a0c71037c0923183b
VMan-2002 <vman.2002.au@gmail.com>
2026-03-29 17:59:58 +1100

    card anim optimizing

--- lua/card_anim.lua
+++ lua/card_anim.lua
@@ -1,10 +1,24 @@
 topuplib.cardAnimations = {}
+local recentchange = true
 
 local updateref = Game.update
 function Game.update(...)
+	if recentchange then
+		recentchange = false
+		topuplib.cardAnimationsActive = {}
+		topuplib.cardAnimationTargets = {}
+		for k,v in pairs(G.I.CARD) do
+			local center = v.config.center
+			if center and center.topuplib_anim then
+				topuplib.cardAnimationsActive[center.topuplib_anim.rate] = true
+				topuplib.cardAnimationTargets[#topuplib.cardAnimationTargets + 1] = v
+			end
+		end
+	end
 	local a = updateref(...)
 	local dt = love.timer.getDelta()
-	for i,tc in pairs(topuplib.cardAnimations) do
+	for av,_ in pairs(topuplib.cardAnimationsActive) do
+		local tc = topuplib.cardAnimations[av]
 		tc[2] = tc[2] + dt
 		if tc[2] >= tc[1] then
 			tc[2] = tc[2] - tc[1]
@@ -15,8 +29,7 @@ function Game.update(...)
 					anm.frameNum = 0
 				end
 				anm.runFunc(center, anm)
-				--TODO: this may not be snail speed but it could be more optimal
-				for k,v in pairs(G.I.CARD) do
+				for k,v in pairs(topuplib.cardAnimationTargets) do
 					if v.config.center_key == key then
 						v:topuplib_update_anim(center, anm)
 					end
@@ -72,4 +85,22 @@ function Card:topuplib_update_anim(center, anm)
 	if self.children.floating_sprite then
 		self.children.floating_sprite:set_sprite_pos(center.soul_pos)
 	end
+end
+
+local card_remove_ref = Card.remove
+function Card:remove(...)
+	recentchange = true
+	return card_remove_ref(self, ...)
+end
+
+local card_set_base_ref = Card.set_base
+function Card:set_base(...)
+	recentchange = true
+	return card_set_base_ref(self, ...)
+end
+
+local card_set_ability_ref = Card.set_ability
+function Card:set_ability(...)
+	recentchange = true
+	return card_set_ability_ref(self, ...)
 end
