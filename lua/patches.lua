5a7f7919a4a4cf4c388463c576be103b57037a68
VMan-2002 <vman.2002.au@gmail.com>
2026-05-02 15:09:17 +1000

    talisman sound fallbacks

--- lua/patches.lua
+++ lua/patches.lua
@@ -18,6 +18,15 @@ function Blind:drawn_to_hand(...)
 end
 
 tul_play_sound_ref = play_sound
+local talisman_fallbacks = next(SMODS.find_mod("Talisman")) and {
+	talisman_echip = "xchips",
+	talisman_eechip = "xchips",
+	talisman_eeechip = "xchips",
+	talisman_emult = "multhit2",
+	talisman_eemult = "multhit2",
+	talisman_eeemult = "multhit2"
+	--dont add talisman_xchip
+} or nil
 function play_sound(name, pitch, vol, ...)
 	if name == "balance" then
 		pitch = pitch or 1
@@ -25,6 +34,8 @@ function play_sound(name, pitch, vol, ...)
 		play_sound('gong', 0.94*1.5*pitch, 0.2*vol, ...)
 		play_sound('tarot1', 1.5*pitch, vol, ...)
 		return play_sound('gong', 0.94*pitch, 0.3*vol, ...)
+	elseif talisman_fallbacks ~= nil and talisman_fallbacks[name] and not SMODS.Sounds[name] then
+		return play_sound(talisman_fallbacks[name], pitch, vol, ...)
 	end
 	return tul_play_sound_ref(name, pitch, vol, ...)
 end
